e7f4217ee54d4d9deb40b86d85848e9796a84401
John Callaway <john@1north.com>
2020-08-19 20:11:54 -0400

    Added:
     - Swagger
     - additional projects,
     - EF
     - configs

--- Example.Api/WeatherForecast.cs
+++ Example.Api/WeatherForecast.cs
@@ -10,6 +10,6 @@ namespace Example.Api
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string Summary { get; set; } = default!;
     }
 }
---
+++ Example.Infrastructure/Data/DefaultContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Example.Infrastructure.Data
+{
+    public class DefaultContext : DbContext
+    {
+        public DefaultContext(DbContextOptions<DefaultContext> options) : base(options)
+        {
+        }
+    }
+}
